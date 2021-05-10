cb78ee8ee5b6170e4c0457fed7a3e55d67a11c29
Valentin Bersier <vbersier@gmail.com>
2021-05-10 07:23:49 +0200

    Added deps and dynamic metadata

--- pancaketrade/__init__.py
+++ pancaketrade/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+try:
+    from importlib.metadata import metadata, version  # type: ignore
+except ModuleNotFoundError:
+    from importlib_metadata import metadata, version  # type: ignore
+
+__version__ = version('pancaketrade')
+__doc__ = metadata('pancaketrade')['Summary']
+__author__ = metadata('pancaketrade')['Author']
