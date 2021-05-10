eec1816fb209b20900d3910c628fbd8f024d77a4
Valentin Bersier <vbersier@gmail.com>
2021-05-10 18:30:44 +0200

    Added models

--- pancaketrade/persistence/models.py
+++ pancaketrade/persistence/models.py
@@ -1,13 +1,25 @@
-from peewee import Model, CharField, FixedCharField
+from peewee import CharField, FixedCharField, ForeignKeyField, Model, SmallIntegerField
 from playhouse.pool import PooledSqliteDatabase
 
 db = PooledSqliteDatabase('pancaketrade.db', max_connections=20, stale_timeout=20, timeout=0)
 
 
 class Token(Model):
-    address = FixedCharField(max_length=42)
+    address = FixedCharField(max_length=42, unique=True)
     symbol = CharField()
-    icon = CharField()
+    icon = CharField(null=True)  # emoji
+    decimals = SmallIntegerField()
+
+    class Meta:
+        database = db
+
+
+class Order(Model):
+    token = ForeignKeyField(Token, backref='orders')
+    type = FixedCharField(max_length=4)  # buy or sell
+    limit_price = CharField()  # store as string
+    trailing_stop = SmallIntegerField(null=True)  # in percent
+    amount = CharField()
 
     class Meta:
         database = db
@@ -15,6 +27,5 @@ class Token(Model):
 
 def init_db() -> None:
     global db
-    db.connect()
-    db.create_tables([Token])
-    db.close()
+    with db:
+        db.create_tables([Token, Order])
