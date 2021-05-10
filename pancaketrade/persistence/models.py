815f05d9dd18dd8898d0c05080849cb2a57271ff
Valentin Bersier <vbersier@gmail.com>
2021-05-10 18:46:29 +0200

    Made gas price optional

--- pancaketrade/persistence/models.py
+++ pancaketrade/persistence/models.py
@@ -22,7 +22,7 @@ class Order(Model):
     trailing_stop = SmallIntegerField(null=True)  # in percent
     amount = CharField()  # in wei, either BNB or token depending on "type"
     slippage = SmallIntegerField()
-    gas_price = CharField()  # in wei
+    gas_price = CharField(null=True)  # in wei, if null then use network gas price
 
     class Meta:
         database = db
