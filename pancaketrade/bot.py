b98a3fec66f7bbe7c6380abdd1bfe4d65bee008a
Valentin Bersier <vbersier@gmail.com>
2021-05-11 08:10:10 +0200

    Added assert to bot commands

--- pancaketrade/bot.py
+++ pancaketrade/bot.py
@@ -15,9 +15,12 @@ def check_chat_id(func: Callable) -> Callable:
     """Compare chat ID with admin's chat ID and refuse access if unauthorized."""
 
     @functools.wraps(func)
-    def wrapper_check_chat_id(tradebot: TradeBot, update: Update, context: CallbackContext, *args, **kwargs):
+    def wrapper_check_chat_id(tradebot, update: Update, context: CallbackContext, *args, **kwargs):
         if update.effective_chat is None:
             logger.debug('No chat ID')
+            return
+        if update.message is None:
+            logger.debug('No message')
             return
         chat_id = update.effective_chat.id
         if chat_id == tradebot.config.secrets.admin_chat_id:
@@ -26,8 +29,6 @@ def check_chat_id(func: Callable) -> Callable:
         context.bot.send_message(
             chat_id=tradebot.config.secrets.admin_chat_id, text=f'Prevented user {chat_id} to interact.'
         )
-        if update.message is None:
-            return
         update.message.reply_text('This bot is not public, you are not allowed to use it.')
 
     return wrapper_check_chat_id
@@ -57,4 +58,5 @@ class TradeBot:
 
     @check_chat_id
     def command_start(self, update: Update, context: CallbackContext):
-        pass
+        assert update.message and update.effective_chat
+        update.message.reply_text('Hi!')
