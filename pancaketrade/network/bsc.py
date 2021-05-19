68b661b67ad079287d142b4ba8dea2d09485897d
Valentin Bersier <vbersier@gmail.com>
2021-05-19 21:12:37 +0200

    Added custom conditional cache for lp addresses

--- pancaketrade/network/bsc.py
+++ pancaketrade/network/bsc.py
@@ -1,6 +1,6 @@
 import time
 from decimal import Decimal
-from typing import NamedTuple, Optional, Set, Tuple
+from typing import Dict, NamedTuple, Optional, Set, Tuple
 
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -14,7 +14,7 @@ from web3.contract import Contract, ContractFunction
 from web3.exceptions import ABIFunctionNotFound, ContractLogicError
 from web3.types import ChecksumAddress, HexBytes, Nonce, TxParams, TxReceipt, Wei
 
-GAS_LIMIT_FAILSAFE = Wei(1000000)  # if the estimated price is above this one, don't use the estimated price
+GAS_LIMIT_FAILSAFE = Wei(1000000)  # if the estimated limit is above this one, don't use the estimated price
 
 
 class NetworkAddresses(NamedTuple):
@@ -58,6 +58,7 @@ class Network:
         self.max_approval_check_int = int(self.max_approval_check_hex, 16)
         self.last_nonce = self.w3.eth.get_transaction_count(self.wallet)
         self.approved: Set[Tuple[str, bool]] = set()  # address and v2 boolean tuples
+        self.lp_cache: Dict[Tuple[str, bool], ChecksumAddress] = {}  # address and v2 boolean tuples as the key
         self.nonce_scheduler = BackgroundScheduler(
             job_defaults={
                 'coalesce': True,
@@ -221,13 +222,17 @@ class Network:
             address=token_address, abi=fetch_abi(contract=token_address, api_key=self.secrets.bscscan_api_key)
         )
 
-    @cached(cache=TTLCache(maxsize=256, ttl=3600))  # cache 60 minutes
     def find_lp_address(self, token_address: ChecksumAddress, v2: bool = False) -> Optional[ChecksumAddress]:
+        cached = self.lp_cache.get((str(token_address), v2))
+        if cached:
+            return cached
         contract = self.contracts.factory_v2 if v2 else self.contracts.factory_v1
         pair = contract.functions.getPair(token_address, self.addr.wbnb).call()
-        if pair == '0x' + 40 * '0':  # not found
+        if pair == '0x' + 40 * '0':  # not found, don't cache
             return None
-        return Web3.toChecksumAddress(pair)
+        checksum_pair = Web3.toChecksumAddress(pair)
+        self.lp_cache[(str(token_address), v2)] = checksum_pair
+        return checksum_pair
 
     def has_both_versions(self, token_address: ChecksumAddress) -> bool:
         lp_v1 = self.find_lp_address(token_address=token_address, v2=False)
